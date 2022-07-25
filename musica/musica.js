6b5913b63519b35319a702b003f2e8f0cce419d3
Hamburda <rares.hamburda@gmail.com>
2022-07-25 16:00:19 +0300

    yayaya

--- musica/musica.js
+++ musica/musica.js
@@ -1,10 +1,8 @@
 function searchDeletes(){
-    let trending = document.getElementById("trending")
     let favorite=document.getElementById("favorite")
     favorite.classList="favorite closeFavorite"
     let musica= document.getElementById("musica")
     musica.innerText="SEARCH RESULTS"
-    trending.style.display="none"
     console.log(favorite)
 }
 function searchClick(){
@@ -12,3 +10,26 @@ function searchClick(){
     const ics=document.getElementById("ics")
     ics.style.display="flex"
 }
+function icsClick(){
+    undoSearchDeletes()
+    const ics=document.getElementById("ics")
+    ics.style.display="none"
+}
+function undoSearchDeletes(){
+    let favorite=document.getElementById("favorite")
+    favorite.classList.remove="closeFavorite"
+    favorite.classList="favorite"
+    let musica= document.getElementById("musica")
+    musica.innerText="MUSICA"
+    console.log(favorite)
+}
+function musicFetch(){
+    fetch("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&format=json" )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      });
+}
+window.addEventListener("load",()=>{
+    musicFetch()
+})
