0db6dbbc15873dfd8a3caea8db50d03c3925267f
Hamburda <rares.hamburda@gmail.com>
2022-08-01 15:37:30 +0300

    syan

--- musica/musica.js
+++ musica/musica.js
@@ -1,140 +1,65 @@
+let arrayOfFavMusic=[]
+let trendingData;
+let trenName
+let favorite, musica
+
 window.addEventListener("load" ,()=>{
+    arrayOfFavMusic=JSON.parse(localStorage.getItem("favorite"))|| []
     const searchS=document.getElementById("search")
     searchS.addEventListener("keyup", musicSearhFetch)
     searchS.addEventListener("click", searchClick)
-    const trenName=document.getElementById("tren")
+    trenName=document.getElementById("tren")
     const ics=document.getElementById("ics")
     ics.addEventListener("click", icsClick)
-    let trendingData;
-
-    function searchDeletes(){
-        let favorite=document.getElementById("favorite")
-        favorite.classList="favorite closeFavorite"
-        let musica= document.getElementById("musica")
-        musica.innerText="SEARCH RESULTS"
-    }
-    function searchClick(){
-        searchDeletes()
-        ics.style.display="flex"
-    }
-    
-    function undoSearchDeletes(){
-        const favorite=document.getElementById("favorite")
-        favorite.classList.remove="closeFavorite"
-        favorite.classList="favorite"
-        const musica= document.getElementById("musica")
-        musica.innerText="MUSICA"
-       document.getElementById("search").value = ""
-    
-    }
-    function musicFetch(){
-        fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&format=json" )
-          .then((res) => res.json())
-          .then((data) => {
-            generateTrending(data)
-            trendingData=data
-          });
-    }
-    function albumFetch(artist, title, img, def){
-        fetch( `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&artist=${artist}&track=${title}&format=json` )
-          .then((res) => res.json())
-          .then((data) => {
-            if(data.track.album){
-                img.src = data.track.album.image[2]["#text"]
-                img.setAttribute("data-album",data.track.album.title)
-            }
-            else{
-                img.src = def
-                img.setAttribute("data-album","Louder actions")
-            }
-            
-          });
-    }
-    
+    favorite = document.getElementById("favorite")
+    musica= document.getElementById("musica")
     musicFetch()
-
-
-    
-    function loopDubios(coolData){
-        const gridTrending= document.getElementById("gridTrending")
-        gridTrending.innerHTML=""
-        for(let i=0;  i<coolData.length; i++){
-            const actualTrend = document.createElement("div")
-            const imgsTrensong = document.createElement("div")
-            let img = document.createElement("img")
-            let songName = document.createElement("p")
-            let artistName = document.createElement("p")
-            img.classList.add("cover")
-            img.classList.add("skeleton")
-            img.addEventListener("error",()=>{
-                img.src="resurse/hill.jpg"
-            })
-
-            albumFetch(coolData[i].artist, coolData[i].name, img, coolData[i].image[2]["#text"])
-
-            songName.classList.add("songname")
-            artistName.classList.add("artist")
-            gridTrending.appendChild(actualTrend)
-            actualTrend.appendChild(imgsTrensong)
-            songName.innerText=coolData[i].name
-            artistName.innerText=coolData[i].artist
-            img.setAttribute("data-title", coolData[i].name)
-            img.setAttribute("data-artist", coolData[i].artist)
-            imgsTrensong.appendChild(img)
-            imgsTrensong.appendChild(songName)
-            imgsTrensong.appendChild(artistName)
-            actualTrend.addEventListener('click', popUpDisplay)
-            actualTrend.addEventListener('click', generatePopUpElem)
-        }
-    }
-    function generateTrending(data, isPhoto){
-        const trendShi = data.tracks.track.map((track)=>({...track,artist:track.artist.name}))
-        loopDubios(trendShi, isPhoto)
-    }
-    function musicSearhFetch(){
-        const value = document.getElementById("search").value
-        fetch("http://ws.audioscrobbler.com/2.0/?method=track.search&track="+value+"&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&format=json" )
-          .then((res) => res.json())
-          .then((data) => {
-            genrateSearchResult(data)
-            // generatePopUpElem(data)
-            console.log(data)
-          });
-    }
-    function genrateSearchResult(data){
-        const searchShi = data.results.trackmatches.track
-        console.log("gvfcgfgccgfcfcfcfvgvg")
-        loopDubios(searchShi)
-        const value = document.getElementById("search").value
-        trenName.innerText="MUSIC WITH "+value
-        trenName.innerText=trenName.innerText.toUpperCase()
-    }
-    function icsClick(){
-        
-        undoSearchDeletes()
-        ics.style.display="none"
-
-        setTimeout(()=>{
-            generateTrending(trendingData)
-            trenName.innerText="TRENDING SONGS"
-        },500)
-        
-    }
-    function generatePopUpElem(e){
-        const pozaFundalPop = document.getElementById("songfoto")
-        const actualPopup = document.getElementById("infoPopUp")
-        const pozaMelodie= document.getElementById("pozaMelodie")
-        const title = document.getElementById("titlename")
-        const artist =document.getElementById("artistname")
-        const album =document.getElementById("albumname")
-        pozaFundalPop.classList.add("songfoto")
-        pozaFundalPop.setAttribute("src" ,e.target.getAttribute("src"))
-        album.innerText=e.target.getAttribute("data-album")
-        title.innerText=e.target.getAttribute("data-title")
-        artist.innerText=e.target.getAttribute("data-artist")
-        pozaMelodie.appendChild(pozaFundalPop)
-    }     
+    loopDubios(arrayOfFavMusic,"actualFavSongs", true)
 })
+
+
+
+
+function searchDeletes(){
+    favorite.classList="favorite closeFavorite"
+    musica.innerText="SEARCH RESULTS"
+}
+function searchClick(){
+    searchDeletes()
+    ics.style.display="flex"
+}
+
+function undoSearchDeletes(){
+    favorite.classList.remove="closeFavorite"
+    favorite.classList="favorite"
+    musica.innerText="MUSICA"
+    document.getElementById("search").value = ""
+}
+
+
+function musicFetch(){
+    fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&format=json" )
+      .then((res) => res.json())
+      .then((data) => {
+        generateTrending(data)
+        trendingData=data
+      });
+}
+function albumFetch(artist, title, img, def){
+    fetch( `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&artist=${artist}&track=${title}&format=json` )
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.track && data.track.album){
+            img.src = data.track.album.image[2]["#text"]
+            img.setAttribute("data-album",data.track.album.title)
+        }
+        else{
+            img.src = def
+            img.setAttribute("data-album","Louder actions")
+        }
+      });
+}
+
 function popUpDisplay(){
         const infoPopUp = document.getElementById("infoPopUp")     
         const backPopUp = document.getElementById("backGroundPopUp")
@@ -152,5 +77,143 @@ function deletePopUp(){
 function playButton(){
     const pozaFundalPop = document.getElementById("fundalPopUp")
     const playButton = document.getElementById("buttonPlay")
-    pozaFundalPop.setAttribute("src", "resurse/dinamic.gif")
+    if(pozaFundalPop.getAttribute("data-gif")==="tru"){
+        pozaFundalPop.setAttribute("src", "resurse/dinamic.gif")
+        pozaFundalPop.setAttribute("data-gif", "fals")
+    }else{
+        pozaFundalPop.setAttribute("src", "resurse/static.png")
+        pozaFundalPop.setAttribute("data-gif", "tru")
+    }
+}
+function changeButton(){
+    const favTxt =document.getElementById("divFavTxt")
+    const divFavTxt =document.getElementById("divFavTxt")
+    if(favTxt.innerText==="Remove from Favorite"){
+        favTxt.innerText="Add to Favorites"
+        divFavTxt.classList.remove("buttonFavoriteRed")
+        
+    }else{
+        favTxt.innerText="Remove from Favorite"
+        divFavTxt.classList.add("buttonFavoriteRed")
+    }
+}
+function saveToArray(title,artist,src,album){
+    if(!arrayOfFavMusic.find(e=>e.name===title)){
+    arrayOfFavMusic.push({name:title,artist:artist,img:src,album:album})
+    }else{
+        let index=arrayOfFavMusic.findIndex(e=>e.name===title)
+        arrayOfFavMusic.splice(index,1)
+    }
+    localStorage.setItem("favorite",JSON.stringify(arrayOfFavMusic))
+    listFav(arrayOfFavMusic)
+    changeButton();
+}
+function listFav(data){
+    loopDubios(data,"actualFavSongs", true)
+}
+
+
+
+function loopDubios(coolData, container, isFav){
+    const gridTrending= document.getElementById(container)
+    gridTrending.innerHTML=""
+    for(let i=0;  i<coolData.length; i++){
+        const root = document.createElement("div")
+        const divContainer = document.createElement("div")
+        const musicImg = document.createElement("img")
+        const songName = document.createElement("p")
+        const artistName = document.createElement("p")
+
+
+        if(!isFav){
+            albumFetch(coolData[i].artist, coolData[i].name, musicImg, coolData[i].image[2]["#text"])
+        }
+        else{
+            musicImg.src = coolData[i].img;
+        }
+        musicImg.classList.add("cover")
+        musicImg.classList.add("skeleton")
+        musicImg.addEventListener("error",()=>{
+            musicImg.src="resurse/hill.jpg"
+        })
+
+        songName.classList.add("songname")
+        artistName.classList.add("artist")
+        root.appendChild(divContainer)
+        songName.innerText=coolData[i].name
+        artistName.innerText=coolData[i].artist
+        musicImg.setAttribute("data-title", coolData[i].name)
+        musicImg.setAttribute("data-artist", coolData[i].artist)
+        divContainer.appendChild(musicImg)
+        divContainer.appendChild(songName)
+        divContainer.appendChild(artistName)
+        root.addEventListener('click', popUpDisplay)
+        root.addEventListener('click', generatePopUpElem)
+        // console.log(root)
+
+
+        gridTrending.appendChild(root)
+    }
+}
+function generateTrending(data){
+    const trendShi = data.tracks.track.map((track)=>({...track,artist:track.artist.name}))
+    loopDubios(trendShi, "gridTrending", false)
+}
+function musicSearhFetch(){
+    const value = document.getElementById("search").value
+    fetch("http://ws.audioscrobbler.com/2.0/?method=track.search&track="+value+"&api_key=ddc75f55ff59c84a1b4f11fefc6313a9&format=json" )
+      .then((res) => res.json())
+      .then((data) => {
+        genrateSearchResult(data)
+        // generatePopUpElem(data)
+      });
+}
+function genrateSearchResult(data){
+    const searchShi = data.results.trackmatches.track
+    loopDubios(searchShi, "gridTrending", false)
+    const value = document.getElementById("search").value
+    trenName.innerText="MUSIC WITH "+value
+    trenName.innerText=trenName.innerText.toUpperCase()
+}
+function icsClick(){
+    
+    undoSearchDeletes()
+    ics.style.display="none"
+
+    setTimeout(()=>{
+        generateTrending(trendingData)
+        trenName.innerText="TRENDING SONGS"
+    },500)
+    
+}
+function generatePopUpElem(e){
+    const favTxt =document.getElementById("favTxt")
+    const divFavTxt =document.getElementById("divFavTxt")
+    const pozaFundalPop = document.getElementById("songfoto")
+    const actualPopup = document.getElementById("infoPopUp")
+    const pozaMelodie= document.getElementById("pozaMelodie")
+    const title = document.getElementById("titlename")
+    const artist =document.getElementById("artistname")
+    const album =document.getElementById("albumname")
+    pozaFundalPop.classList.add("songfoto")
+    pozaFundalPop.setAttribute("src" ,e.target.getAttribute("src"))
+    album.innerText=e.target.getAttribute("data-album")
+    title.innerText=e.target.getAttribute("data-title")
+    artist.innerText=e.target.getAttribute("data-artist")
+    pozaMelodie.appendChild(pozaFundalPop)
+    const _name = e.target.getAttribute("data-title")
+    const _artist = e.target.getAttribute("data-artist")
+    const _album = e.target.getAttribute("data-album")
+
+    if(arrayOfFavMusic.find(f=>f.name===_name)){
+        divFavTxt.classList.add("buttonFavoriteRed")
+        divFavTxt.innerText="Remove from Favorite"
+    }else{
+        divFavTxt.classList.remove("buttonFavoriteRed")
+        divFavTxt.innerText="Add to Favorite"
+    }
+    divFavTxt.onclick =()=>{
+        saveToArray(_name,_artist,e.target.src,_album)
+    }
+
 }
