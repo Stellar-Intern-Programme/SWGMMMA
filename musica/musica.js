9d4ac1d9424290570b0874a26a67e01409ae6c89
Fiils <ioanalexandruipatov12@gmail.com>
2022-08-09 11:44:58 +0300

    fixed

--- musica/musica.js
+++ musica/musica.js
@@ -30,17 +30,6 @@ window.addEventListener('load', () => {
   });
 });
 
-window.addEventListener('message', e => {
-  document.getElementById('musica').innerText = 'asd';
-  localStorage.setItem('favorite', JSON.stringify(e.data));
-  arrayOfFavMusic = JSON.parse(e.data) || [];
-  loopDubios(arrayOfFavMusic, 'actualFavSongs', true);
-
-  if (arrayOfFavMusic.length === 0) {
-    creeazaPoza();
-  }
-});
-
 function creeazaPoza() {
   const actual = document.getElementById('actualFavSongs');
   const poza = document.createElement('img');
